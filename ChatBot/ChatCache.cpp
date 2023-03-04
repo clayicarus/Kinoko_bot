a9907f080499d06dd0de6d6920e35bd526a031d9
clayicarus <1072464850@qq.com>
2023-03-04 21:49:31 +0800

    first version of ChatBot cache

--- ChatBot/ChatCache.cpp
+++ ChatBot/ChatCache.cpp
@@ -3,3 +3,35 @@
 //
 
 #include "ChatCache.h"
+
+const std::string &ChatCache::getPrompt()
+{
+    if(changed()) {
+        makePrompt();
+    }
+    return prompt_;
+}
+
+void ChatCache::makePrompt()
+{
+    Content::size_type curSz = 0;
+    Content res;
+    for(const auto &i : cache_) {
+        if(i.second.size() + curSz <= maxPromptSize()) {
+            curSz += i.second.size();
+            res.append(i.second);
+        } else {
+            return;
+        }
+    }
+}
+
+void ChatCache::insert(const ChatCache::Speaker &speaker, const ChatCache::Content &content)
+{
+    cache_.emplace_front(speaker, content);
+    if(content.size() + currentPromptSize() > maxPromptSize()) {
+        changed() = true;
+    } else {
+        prompt_.append(content);
+    }
+}
--- ChatBot/ChatCache.h
+++ ChatBot/ChatCache.h
@@ -6,8 +6,33 @@
 #define ATRI_CHATCACHE_H
 
 
-class ChatCache {
+#include <string>
+#include <deque>
 
+class ChatCache {
+    typedef std::string Speaker;
+    typedef std::string Content;
+    typedef std::deque<std::pair<Speaker, Content>> Cache;  // push_front, pop_back
+public:
+    const std::string& getPrompt();
+    void insert(const Speaker &speaker, const Content &content);
+
+    const Content::size_type &maxPromptSize() const { return maxPromptSize_; }
+    Content::size_type &maxPromptSize() { return maxPromptSize_; }
+
+    Content::size_type currentPromptSize() const { return prompt_.size(); }
+private:
+    Content::size_type maxPromptSize_;
+    Cache cache_;
+    Content prompt_;
+
+    bool& changed() { return changed_; }
+    const bool& changed() const { return changed_; }
+    bool changed_;
+
+    void makePrompt();
+
+    void printCurrentPrompt() const { printf("%s", prompt_.c_str()); }
 };
 
 
