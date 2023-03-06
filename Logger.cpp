a9a8193af6e5a66b60959271ce842f1d34dfa473
clayicarus <1072464850@qq.com>
2023-03-07 01:13:45 +0800

    simple logger

---
+++ Logger.cpp
@@ -0,0 +1,5 @@
+//
+// Created by clay on 3/6/23.
+//
+
+#include "Logger.h"
---
+++ Logger.h
@@ -0,0 +1,30 @@
+//
+// Created by clay on 3/6/23.
+//
+
+#ifndef ATRI_LOGGER_H
+#define ATRI_LOGGER_H
+
+
+#include <string>
+
+class Logger {
+public:
+    static constexpr char DEBUG[] = "[DEBUG]";
+    static constexpr char INFO[] = "[INFO]";
+    static constexpr char WARN[] = "[WARN]";
+    static constexpr char ERROR[] = "[ERROR]";
+    static constexpr char FATAL[] = "[FATAL]";
+    Logger(const char *level, const char *file, int line, const std::string &info) {
+        printf("%s [%s:%d]  %s\n", level, file, line, info.c_str());
+        if(level == FATAL)
+            abort();
+    }
+};
+#define LOG_DEBUG(debug) Logger(Logger::DEBUG, __FILE__, __LINE__, debug)
+#define LOG_INFO(info) Logger(Logger::INFO, __FILE__, __LINE__, info)
+#define LOG_WARN(warn) Logger(Logger::WARN, __FILE__, __LINE__, warn)
+#define LOG_ERROR(error) Logger(Logger::ERROR, __FILE__, __LINE__, error)
+#define LOG_FATAL(fatal) Logger(Logger::FATAL, __FILE__, __LINE__, fatal)
+
+#endif //ATRI_LOGGER_H
