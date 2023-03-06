a94cab85b0ca02f55d5511c46a483ed7308cb29e
clayicarus <1072464850@qq.com>
2023-03-07 01:20:42 +0800

    first version

--- ChatBot/ChatBot.cpp
+++ ChatBot/ChatBot.cpp
@@ -3,3 +3,54 @@
 //
 
 #include "ChatBot.h"
+#include "../OpenAI_API/OpenAI_API.h"
+#include "../Logger.h"
+
+std::string ChatBot::getOneReply(const std::string &name)
+{
+    if(messages_.empty()) {
+        LOG_FATAL("MessageQueue empty");
+    }
+    auto res = messages_.back();
+    messages_.pop_back();
+    res = res.substr(name.size() + strlen(": "));
+    return res;
+}
+
+void ChatBot::speak(const std::string &speaker, const std::string &content)
+{
+    auto talk = speaker + ": " + content + "\n";
+    auto prompt = scene_ + cache_.getCacheString(3000) + talk;
+    setSpeakers(speaker);
+    parameter_.setPrompt(prompt);
+
+    std::string res;
+    LOG_INFO("createCompletion");
+    auto r = OpenAI_API::createCompletion(parameter_.getPara());
+    LOG_INFO("createCompletion finished");
+    if(!r["is_error"]) {
+        if(r["status_code"] == 200) {
+            res = r["response_data"]["choices"][0]["text"];
+            talk.append(res + '\n');
+            cache_.insert(speaker, talk);
+        } else {
+            LOG_ERROR("API response error");
+            res = "[API Error]: " + r["response_data"].dump();
+        }
+    } else {
+        LOG_ERROR("cpr error");
+        std::string err_msg = r["error_message"];
+        res = "[Network Error]: " + err_msg;
+    }
+    messages_.push_front(res);
+}
+
+void ChatBot::setSpeakers(const std::string &speaker)
+{
+    auto temp(speaker);
+    temp.push_back('\n');
+    temp = "\n" + temp + ": ";
+    auto self = "\n" + name_ + ": ";
+    parameter_.setStop({self, temp});
+}
+
--- ChatBot/ChatBot.h
+++ ChatBot/ChatBot.h
@@ -17,20 +17,32 @@ class ChatCache;
 
 class ChatBot {
 public:
-    ChatBot();
-    void speak(const std::string &name, const std::string &content);
-    std::string&& getOneReply(const std::string &name);
+    ChatBot()
+        : scene_("The following is a conversation with an AI assistant. "
+                 "The assistant is helpful, creative, clever, and very friendly. "
+                 "\n\nHuman: Hello, who are you?\n"
+                 "Atri: I am Atri, an AI realized with GPT. How can Atri help you today?\n"
+                 "Human: 你可以说中文吗？\n"
+                 "Atri: 当然可以，Atri有什么可以帮助您？\n"),
+          name_("Atri"),
+          cache_(3000)
+    {}
+    void speak(const std::string &speaker, const std::string &content);
+    std::string getOneReply(const std::string &name);
 
 private:
-    ChatCache cache;
-    ChatMemory memory;
+    void setSpeakers(const std::string &speaker);
 
-    const std::string botName;
-    std::string scene;
-    std::set<std::string> currentSpeaker;
+    typedef std::deque<std::string> MessageQueue;
+    MessageQueue messages_;
 
-    CompletionParameter parameter;
-    std::string debugResponse;
+    ChatCache cache_;
+
+    const std::string name_;
+    std::string scene_;
+    std::set<std::string> currentSpeaker_;
+
+    CompletionParameter parameter_;
 };
 
 #endif //ATRI_CHATBOT_H
