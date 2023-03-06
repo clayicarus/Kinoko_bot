1a53e7ecfd8089433557053122ac5e8246513bb0
clayicarus <1072464850@qq.com>
2023-03-07 01:19:23 +0800

    first version

--- ChatBot/CompletionParameter.cpp
+++ ChatBot/CompletionParameter.cpp
@@ -3,3 +3,13 @@
 //
 
 #include "CompletionParameter.h"
+
+const nlohmann::json &CompletionParameter::getPara()
+{
+    para_["model"] = model_;
+    para_["max_tokens"] = max_tokens_;
+    para_["temperature"] = temperature_;
+    para_["frequency_penalty"] = frequency_penalty_;
+    para_["presence_penalty"] = presence_penalty_;
+    return para_;
+}
--- ChatBot/CompletionParameter.h
+++ ChatBot/CompletionParameter.h
@@ -5,12 +5,35 @@
 #ifndef ATRI_COMPLETIONPARAMETER_H
 #define ATRI_COMPLETIONPARAMETER_H
 
+#include <nlohmann/json.hpp>
+#include <string>
 
 class CompletionParameter {
-    float temperature;
-    unsigned max_tokens;
-    float frequency_penalty;
-    float presence_penalty;
+    friend class ChatBot;
+public:
+    CompletionParameter() :
+        model_("text-davinci-003"),
+        max_tokens_(200),
+        temperature_(.3f),
+        frequency_penalty_(.5f),
+        presence_penalty_(.5)
+    {}
+    [[nodiscard]] const nlohmann::json &getPara();
+    unsigned &max_tokens() { return max_tokens_; }
+    float &temperature() { return temperature_; }
+    float &frequency_penalty() { return frequency_penalty_; }
+    float &presence_penalty() { return presence_penalty_; }
+    std::string &model() { return model_; }
+private:
+    std::string model_;
+    unsigned max_tokens_;
+    float temperature_;
+    float frequency_penalty_;
+    float presence_penalty_;
+    nlohmann::json para_;
+
+    void setPrompt(const std::string &prompt) { para_["prompt"] = prompt; }
+    void setStop(const std::vector<std::string> &stop) { para_["stop"] = stop; }
 };
 
 
